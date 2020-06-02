f5b9ddcfe8ef4cda8b9a0f2e11da4b5a91aa4deb
Kevin Blaszczyk <OrcHound@gmail.com>
2020-06-02 23:07:54 +0200

    Make shader class JOML independent.

--- src/main/kotlin/de/orchound/rendering/shaderutility/OpenGLShader.kt
+++ src/main/kotlin/de/orchound/rendering/shaderutility/OpenGLShader.kt
@@ -1,6 +1,5 @@
 package de.orchound.rendering.shaderutility
 
-import org.joml.*
 import org.lwjgl.opengl.GL20.*
 import java.nio.FloatBuffer
 
@@ -8,16 +7,16 @@ class OpenGLShader(
 	private val programHandle: Int, uniformProvider: UniformProvider
 ) : Shader {
 
-	private val intSetterByUniform: Map<String, (Int) -> Unit> = uniformProvider.getUniforms("int")
+	private val samplerSetterByUniform: Map<String, (Int) -> Unit> = uniformProvider.getUniformTypes()
+		.filter { it.startsWith("sampler") }
+		.flatMap { uniformProvider.getUniforms(it) }
 		.map { uniform -> uniform to glGetUniformLocation(programHandle, uniform) }
 		.filterNot { (_, location) -> location == -1 }
 		.associate { (uniform, location) ->
 			uniform to { value: Int -> setIntUniform(location, value) }
 		}
 
-	private val samplerSetterByUniform: Map<String, (Int) -> Unit> = uniformProvider.getUniformTypes()
-		.filter { it.startsWith("sampler") }
-		.flatMap { uniformProvider.getUniforms(it) }
+	private val intSetterByUniform: Map<String, (Int) -> Unit> = uniformProvider.getUniforms("int")
 		.map { uniform -> uniform to glGetUniformLocation(programHandle, uniform) }
 		.filterNot { (_, location) -> location == -1 }
 		.associate { (uniform, location) ->
@@ -35,46 +34,43 @@ class OpenGLShader(
 		.map { uniform -> uniform to glGetUniformLocation(programHandle, uniform) }
 		.filterNot { (_, location) -> location == -1 }
 		.associate { (uniform, location) ->
-			uniform to { values: FloatBuffer -> setFloatArrayUniform(location, values) }
+			uniform to { values: FloatBuffer -> setFloatBufferUniform(location, values) }
 		}
 
-	private val vec2SetterByUniform: Map<String, (Vector2f) -> Unit> = uniformProvider.getUniforms("vec2")
+	private val vec2SetterByUniform = uniformProvider.getUniforms("vec2")
 		.map { uniform -> uniform to glGetUniformLocation(programHandle, uniform) }
 		.filterNot { (_, location) -> location == -1 }
 		.associate { (uniform, location) ->
-			uniform to { vector: Vector2f -> setVec2Uniform(location, vector) }
+			uniform to { x: Float, y: Float -> setFloat2Uniform(location, x, y) }
 		}
 
-	private val vec3SetterByUniform: Map<String, (Vector3f) -> Unit> = uniformProvider.getUniforms("vec3")
+	private val vec3SetterByUniform = uniformProvider.getUniforms("vec3")
 		.map { uniform -> uniform to glGetUniformLocation(programHandle, uniform) }
 		.filterNot { (_, location) -> location == -1 }
 		.associate { (uniform, location) ->
-			uniform to { vector: Vector3f -> setVec3Uniform(location, vector) }
+			uniform to { x: Float, y: Float, z: Float -> setFloat3Uniform(location, x, y, z) }
 		}
 
-	private val vec4SetterByUniform: Map<String, (Vector4f) -> Unit> = uniformProvider.getUniforms("vec4")
+	private val vec4SetterByUniform = uniformProvider.getUniforms("vec4")
 		.map { uniform -> uniform to glGetUniformLocation(programHandle, uniform) }
 		.filterNot { (_, location) -> location == -1 }
 		.associate { (uniform, location) ->
-			uniform to { vector: Vector4f -> setVec4Uniform(location, vector) }
+			uniform to { x: Float, y: Float, z: Float, w: Float -> setFloat4Uniform(location, x, y, z, w) }
 		}
 
-	private val mat3SetterByUniform: Map<String, (Matrix3f) -> Unit> = uniformProvider.getUniforms("mat3")
+	private val mat3SetterByUniform = uniformProvider.getUniforms("mat3")
 		.map { uniform -> uniform to glGetUniformLocation(programHandle, uniform) }
 		.filterNot { (_, location) -> location == -1 }
 		.associate { (uniform, location) ->
-			uniform to { matrix: Matrix3f -> setMat3Uniform(location, matrix) }
+			uniform to { matrix: FloatBuffer -> setFloatBufferUniform(location, matrix) }
 		}
 
-	private val mat4SetterByUniform: Map<String, (Matrix4f) -> Unit> = uniformProvider.getUniforms("mat4")
+	private val mat4SetterByUniform = uniformProvider.getUniforms("mat4")
 		.map { uniform -> uniform to glGetUniformLocation(programHandle, uniform) }
 		.filterNot { (_, location) -> location == -1 }
 		.associate { (uniform, location) ->
-			uniform to { matrix: Matrix4f -> setMat4Uniform(location, matrix) }
+			uniform to { matrix: FloatBuffer -> setMat4Uniform(location, matrix) }
 		}
-
-	private val matrix3Buffer = FloatArray(9)
-	private val matrix4Buffer = FloatArray(16)
 
 	override fun bind() = glUseProgram(programHandle)
 	override fun unbind() = glUseProgram(0)
@@ -90,41 +86,29 @@ class OpenGLShader(
 		glBindTexture(GL_TEXTURE_2D, handle)
 	}
 
-	fun getIntSetter(uniform: String): (Int) -> Unit =
-		intSetterByUniform.getOrDefault(uniform) { Unit }
-	fun getFloatSetter(uniform: String): (Float) -> Unit =
-		floatSetterByUniform.getOrDefault(uniform) { Unit }
-	fun getFloatArraySetter(uniform: String): (FloatBuffer) -> Unit =
-		floatArraySetterByUniform.getOrDefault(uniform) { Unit }
-	fun getVector2Setter(uniform: String): (Vector2f) -> Unit =
-		vec2SetterByUniform.getOrDefault(uniform) { Unit }
-	fun getVector3Setter(uniform: String): (Vector3f) -> Unit =
-		vec3SetterByUniform.getOrDefault(uniform) { Unit }
-	fun getVector4Setter(uniform: String): (Vector4f) -> Unit =
-		vec4SetterByUniform.getOrDefault(uniform) { Unit }
-	fun getMatrix3Setter(uniform: String): (Matrix3f) -> Unit =
-		mat3SetterByUniform.getOrDefault(uniform) { Unit }
-	fun getMatrix4Setter(uniform: String): (Matrix4f) -> Unit =
-		mat4SetterByUniform.getOrDefault(uniform) { Unit }
-	fun getSamplerSetter(uniform: String): (Int) -> Unit =
-		samplerSetterByUniform.getOrDefault(uniform) { Unit }
+	fun getSamplerSetter(uniform: String) = samplerSetterByUniform.getOrDefault(uniform) { Unit }
+	fun getIntSetter(uniform: String) = intSetterByUniform.getOrDefault(uniform) { Unit }
+	fun getFloatSetter(uniform: String) = floatSetterByUniform.getOrDefault(uniform) { Unit }
+	fun getFloatArraySetter(uniform: String) = floatArraySetterByUniform.getOrDefault(uniform) { Unit }
+
+	fun getVec2Setter(uniform: String) = vec2SetterByUniform.getOrDefault(uniform) { _, _ -> Unit }
+	fun getVec3Setter(uniform: String) = vec3SetterByUniform.getOrDefault(uniform) { _, _, _ ->  Unit }
+	fun getVec4Setter(uniform: String) = vec4SetterByUniform.getOrDefault(uniform) { _, _, _, _ ->  Unit }
+
+	fun getMat3Setter(uniform: String) = mat3SetterByUniform.getOrDefault(uniform) { Unit }
+	fun getMat4Setter(uniform: String) = mat4SetterByUniform.getOrDefault(uniform) { Unit }
 
 	private fun setIntUniform(uniform: Int, value: Int) = glUniform1i(uniform, value)
+
 	private fun setFloatUniform(uniform: Int, value: Float) = glUniform1f(uniform, value)
-	private fun setVec2Uniform(uniform: Int, value: Vector2f) = glUniform2f(uniform, value.x, value.y)
-	private fun setVec3Uniform(uniform: Int, value: Vector3f) = glUniform3f(uniform, value.x, value.y, value.z)
-	private fun setVec4Uniform(uniform: Int, value: Vector4f) = glUniform4f(uniform, value.x, value.y, value.z, value.w)
+	private fun setFloat2Uniform(uniform: Int, x: Float, y: Float) = glUniform2f(uniform, x, y)
+	private fun setFloat3Uniform(uniform: Int, x: Float, y: Float, z: Float) = glUniform3f(uniform, x, y, z)
+	private fun setFloat4Uniform(uniform: Int, x: Float, y: Float, z: Float, w: Float) =
+		glUniform4f(uniform, x, y, z, w)
+	private fun setFloatBufferUniform(uniform: Int, values: FloatBuffer) = glUniform1fv(uniform, values)
 
-	private fun setFloatArrayUniform(uniform: Int, values: FloatBuffer) = glUniform1fv(uniform, values)
-	private fun setVector3ArrayUniform(uniform: Int, vectors: FloatBuffer) = glUniform3fv(uniform, vectors)
-
-	private fun setMat3Uniform(uniform: Int, matrix: Matrix3f) {
-		matrix.get(matrix3Buffer)
-		glUniformMatrix3fv(uniform, false, matrix3Buffer)
-	}
-
-	private fun setMat4Uniform(uniform: Int, matrix: Matrix4f) {
-		matrix.get(matrix4Buffer)
-		glUniformMatrix4fv(uniform, false, matrix4Buffer)
-	}
+	private fun setMat3Uniform(uniform: Int, matrix: FloatBuffer) =
+		glUniformMatrix3fv(uniform, false, matrix)
+	private fun setMat4Uniform(uniform: Int, matrix: FloatBuffer) =
+		glUniformMatrix4fv(uniform, false, matrix)
 }
